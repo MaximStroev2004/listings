bcea255449ca4d8d4881b19887a840c7ba206906
MaximStroev2004 <120455884+MaximStroev2004@users.noreply.github.com>
2023-02-10 01:01:42 +0300

    Create AlphabetWhileDemo.java

---
+++ Блок 2/AlphabetWhileDemo.java
@@ -0,0 +1,12 @@
+public class AlphabetWhileDemo {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        while (ch <= 'я') {
+            System.out.println(ch);
+            ch++;
+        } // while
+    } // main (String[]) method
+
+} // AlphabetWhileDemo class
