f09c595b34eb443578f3c96540e2c110b5953a76
MaximStroev2004 <120455884+MaximStroev2004@users.noreply.github.com>
2023-02-10 02:24:09 +0300

    Create VehicleRetMethod.java

---
+++ Блок 3/VehicleRetMethod.java
@@ -0,0 +1,27 @@
+public class VehicleRetMethod {
+    public static void main(String[] args) {
+        Vehicle car = new Vehicle();
+        car.passengers = 2;
+        car.wheels = 4;
+        car.maxspeed = 130;
+        car.burnup = 30;
+
+        // другой экземпляр класса Vehicle
+        Vehicle bus = new Vehicle();
+        bus.passengers = 45;
+        bus.wheels = 4;
+        bus.maxspeed = 100;
+        bus.burnup = 25;
+
+        // расчет пути, пройденного за 0.5 часа
+        double time = 0.5;
+        double distanceCar = car.distance(time);
+        double distanceBus = bus.distance(time);
+
+        System.out.print("автомобиль с" + car.passengers + "пассажираси");
+        System.out.println("пройдет за полчаса путь" + distanceCar + " км.");
+        System.out.print("автомобиль с" + bus.passengers + "пассажираси");
+        System.out.println("пройдет за полчаса путь" + distanceBus + " км.");
+
+    } // main (Sting[])
+} // VehicleRetMethod class
