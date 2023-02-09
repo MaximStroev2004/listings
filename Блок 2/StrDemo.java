bccd306f0ab7091c97f6bc5eced6198d38ede839
MaximStroev2004 <120455884+MaximStroev2004@users.noreply.github.com>
2023-02-10 01:02:13 +0300

    Create StrDemo.java

---
+++ Блок 2/StrDemo.java
@@ -0,0 +1,14 @@
+public class StrDemo {
+    public static void main (String[] args) {
+
+        // для перевода каретки (начала новой строки)
+        // служит код \n
+        System.out.println("Первая строка\nВторая строка");
+
+        // знак табуляции помогает оформить выводимый текст
+        // в колонки. Он вводиться с помощью кода \е
+        System.out.println("A\tB\tC");
+        System.out.println("D\tE\tF");
+
+    } // main(String[]) method
+} // StrDemo class
