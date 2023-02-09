71ba072f6dc89f07b917924762c0c7f2fdf0e07d
MaximStroev2004 <120455884+MaximStroev2004@users.noreply.github.com>
2023-02-10 01:02:01 +0300

    Create ForStatementDemo.java

---
+++ Блок 2/ForStatementDemo.java
@@ -0,0 +1,16 @@
+// демонстрация цикла for
+// сохраните исходный код в файл с именем
+// ForStatement.Demo.java
+public class ForStatementDemo {
+
+    public static void main(String[]args) {
+
+        int count;
+
+        for (count = 0; count < 5; count = count + 1)
+            System.out.println("Переменная цикла равна" + 1);
+
+        System.out.println("Цикл окончен");
+
+    } // main(String[]) method
+} // ForStatementDemo class
