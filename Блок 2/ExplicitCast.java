51bffb40578369c9e513912c0fb16b679e4707bf
MaximStroev2004 <120455884+MaximStroev2004@users.noreply.github.com>
2023-02-10 01:01:56 +0300

    Create ExplicitCast.java

---
+++ Блок 2/ExplicitCast.java
@@ -0,0 +1,10 @@
+public class ExplicitCast {
+
+    public static void main(String[] args) {
+
+        long I = 10;
+        double d = 1; // неявное приведение (long в double)
+        I = (long) d; // явное приведение (double в long)
+
+    } // main(String[])
+} // ExplicitCast class
