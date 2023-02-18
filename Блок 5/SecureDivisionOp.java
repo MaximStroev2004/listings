681706539bbddccf08a03a075fec6e3eabc340f8
MaxXxim <120455884+MaximStroev2004@users.noreply.github.com>
2023-02-18 11:47:52 +0300

    Create SecureDivisionOp.java

---
+++ Блок 5/SecureDivisionOp.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class SecureDivisionOp {
+
+    public static void main (String[] args) {
+        int result;
+
+        for (int divisor = -5; divisor <= 5; divisor++)
+        {
+            // использование тернарного оператора для защиты
+            // от деления на ноль
+            result = divisor != 0 ? 100 / divisor : 0;
+            if (result != 0) {
+                System.out.println("100/" + divisor + " = " + result);
+            }
+        }
+    } // main (String[]) method
+} // SecureDivishionOp class
